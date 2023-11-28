f08af2a431d39bcf1dcb8be2d4a467ceb1a41ff0
mateus dias <mateusdias.her@gmail.com>
2023-11-28 14:19:26 -0300

    atualizacao de codigo com foco em gerenciamento de estado com triple

--- lib/features/sign_in/app/src/presenter/sign_in_page.dart
+++ lib/features/sign_in/app/src/presenter/sign_in_page.dart
@@ -1,6 +1,10 @@
 import 'package:auth/features/sign_in/app/src/presenter/sign_in_controller.dart';
+import 'package:auth/features/sign_in/app/src/presenter/sign_in_store.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_triple/flutter_triple.dart';
+
+import '../domain/entity/user_response.dart';
 
 class SignInPage extends StatefulWidget {
   @override
@@ -13,39 +17,69 @@ class _SignInPage extends State<SignInPage> {
   final TextEditingController passwordController = TextEditingController();
 
   @override
+  void initState() {
+    controller.store.observer(onState: (state) {
+      Modular.to.navigate('/');
+    }, onError: (error) {
+      const Center(child: Text('error'));
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Login'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                labelText: 'Username',
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-              ),
-            ),
-            const SizedBox(height: 24.0),
-            ElevatedButton(
-              onPressed: () {
-                controller.sign(emailController.text, passwordController.text);
-              },
-              child: const Text('Login'),
-            ),
-          ],
-        ),
+      body: SingleChildScrollView(
+        child: ScopedBuilder<SignInStore, UserResponse>(
+            store: controller.store,
+            onLoading: (_) => SizedBox(
+                  height: height,
+                  child: const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ),
+            onError: (_, error) => SizedBox(
+                  height: height,
+                  child: const Center(
+                    child: Text('erro'),
+                  ),
+                ),
+            onState: (_, state) {
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextField(
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                        labelText: 'Username',
+                      ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                      ),
+                    ),
+                    const SizedBox(height: 24.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        controller.sign(
+                            emailController.text, passwordController.text);
+                      },
+                      child: const Text('Login'),
+                    ),
+                  ],
+                ),
+              );
+            }),
       ),
     );
   }
--- lib/features/sign_in/sign_in_module.dart
+++ lib/features/sign_in/sign_in_module.dart
@@ -1,5 +1,6 @@
 import 'package:auth/features/sign_in/app/src/domain/repository/sign_in_repository.dart';
 import 'package:auth/features/sign_in/app/src/domain/use_cases/sign_in_usecase.dart';
+import 'package:auth/features/sign_in/app/src/infra/datasources/sign_in_datasource.dart';
 import 'package:auth/features/sign_in/app/src/infra/repository/sign_in_repository_impl.dart';
 import 'package:auth/features/sign_in/app/src/presenter/sign_in_controller.dart';
 import 'package:auth/features/sign_in/app/src/presenter/sign_in_page.dart';
@@ -8,6 +9,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 
 class SignInModule extends Module {
   void binds(i) {
+    i.addSingleton<SignInDataSource>(SignInRepositoryImpl.new);
     i.addSingleton<SignInRepository>(SignInRepositoryImpl.new);
     i.addSingleton(SignInUseCase.new);
     i.addSingleton(SignInStore.new);
